ba470da225c1dfefa8b2936db273d85a0ee82d78
cosmicappleknife <cosmic.appleknife@gmail.com>
2026-07-19 21:26:50 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,31 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 });
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 300) {
+
+        backToTop.classList.add("show");
+
+    } else {
+
+        backToTop.classList.remove("show");
+
+    }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top: 0,
+
+        behavior: "smooth"
+
+    });
+
+});
