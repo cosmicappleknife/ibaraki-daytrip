e46e049c651d8c24c401d7b6226770181cbd81c5
cosmicappleknife <cosmic.appleknife@gmail.com>
2026-07-19 16:28:22 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
+const observer = new IntersectionObserver((entries) => {
 
+  entries.forEach(entry => {
+
+    if (entry.isIntersecting) {
+
+      entry.target.classList.add("show");
+
+    }
+
+  });
+
+}, {
+
+  threshold: 0.15
+
+});
+
+document.querySelectorAll(".fade").forEach(el => {
+
+  observer.observe(el);
+
+});
